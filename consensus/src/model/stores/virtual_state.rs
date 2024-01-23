kaspanet/rusty-kaspa

Add "last known good" cache for virtual state (#399)
* make a better estimation of header count (during IBD this will now count for the headers phase)

* rpc service responsiveness: use `transaction_count_sample` for inferring mempool size

* use counters snapshot

* introduce last known good virtual state cache

* move processing counters to consensus core and remove rpc service crate dep on consensus

* extract consensus stats to a single call

* turn some consensus session calls into non-async + fix get_block_dag_info_call

* use saturating_sub instead of checked_sub + unwrap_or_default

* use join over all async calls

---------

Co-authored-by: coderofstuff <114628839+coderofstuff@users.noreply.github.com>

---------

Co-authored-by: coderofstuff <114628839+coderofstuff@users.noreply.github.com>

consensus/src/model/stores/virtual_state.rs
@@ -1,10 +1,13 @@
+use std::ops::Deref;
 use std::sync::Arc;
 
+use arc_swap::ArcSwap;
+use kaspa_consensus_core::api::stats::VirtualStateStats;
 use kaspa_consensus_core::{
     block::VirtualStateApproxId, coinbase::BlockRewardData, config::genesis::GenesisBlock, tx::TransactionId,
     utxo::utxo_diff::UtxoDiff, BlockHashMap, BlockHashSet, HashMapCustomHasher,
 };
-use kaspa_database::prelude::{BatchDbWriter, CachedDbItem, DirectDbWriter};
+use kaspa_database::prelude::{BatchDbWriter, CachedDbItem, DirectDbWriter, StoreResultExtensions};
 use kaspa_database::prelude::{CachePolicy, StoreResult};
 use kaspa_database::prelude::{StoreError, DB};
 use kaspa_database::registry::DatabaseStorePrefixes;
@@ -77,6 +80,53 @@ impl VirtualState {
     }
 }
 
+impl From<&VirtualState> for VirtualStateStats {
+    fn from(state: &VirtualState) -> Self {
+        Self {
+            num_parents: state.parents.len() as u32,
+            daa_score: state.daa_score,
+            bits: state.bits,
+            past_median_time: state.past_median_time,
+        }
+    }
+}
+
+/// Represents the "last known good" virtual state. To be used by any logic which does not want to wait
+/// for a possible virtual state write to complete but can rather settle with the last known state
+#[derive(Clone, Default)]
+pub struct LkgVirtualState {
+    inner: Arc<ArcSwap<VirtualState>>,
+}
+
+/// Guard for accessing the last known good virtual state (lock-free)
+/// It's a simple newtype over arc_swap::Guard just to avoid explicit dependency
+pub struct LkgVirtualStateGuard(arc_swap::Guard<Arc<VirtualState>>);
+
+impl Deref for LkgVirtualStateGuard {
+    type Target = Arc<VirtualState>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl LkgVirtualState {
+    /// Provides a temporary borrow to the last known good virtual state.
+    pub fn load(&self) -> LkgVirtualStateGuard {
+        LkgVirtualStateGuard(self.inner.load())
+    }
+
+    /// Loads the last known good virtual state.
+    pub fn load_full(&self) -> Arc<VirtualState> {
+        self.inner.load_full()
+    }
+
+    // Kept private in order to make sure it is only updated by DbVirtualStateStore
+    fn store(&self, virtual_state: Arc<VirtualState>) {
+        self.inner.store(virtual_state)
+    }
+}
+
 /// Used in order to group virtual related stores under a single lock
 pub struct VirtualStores {
     pub state: DbVirtualStateStore,
@@ -84,9 +134,9 @@ pub struct VirtualStores {
 }
 
 impl VirtualStores {
-    pub fn new(db: Arc<DB>, utxoset_cache_policy: CachePolicy) -> Self {
+    pub fn new(db: Arc<DB>, lkg_virtual_state: LkgVirtualState, utxoset_cache_policy: CachePolicy) -> Self {
         Self {
-            state: DbVirtualStateStore::new(db.clone()),
+            state: DbVirtualStateStore::new(db.clone(), lkg_virtual_state),
             utxo_set: DbUtxoSetStore::new(db, utxoset_cache_policy, DatabaseStorePrefixes::VirtualUtxoset.into()),
         }
     }
@@ -106,15 +156,20 @@ pub trait VirtualStateStore: VirtualStateStoreReader {
 pub struct DbVirtualStateStore {
     db: Arc<DB>,
     access: CachedDbItem<Arc<VirtualState>>,
+    /// The "last known good" virtual state
+    lkg_virtual_state: LkgVirtualState,
 }
 
 impl DbVirtualStateStore {
-    pub fn new(db: Arc<DB>) -> Self {
-        Self { db: Arc::clone(&db), access: CachedDbItem::new(db, DatabaseStorePrefixes::VirtualState.into()) }
+    pub fn new(db: Arc<DB>, lkg_virtual_state: LkgVirtualState) -> Self {
+        let access = CachedDbItem::new(db.clone(), DatabaseStorePrefixes::VirtualState.into());
+        // Init the LKG cache from DB store data
+        lkg_virtual_state.store(access.read().unwrap_option().unwrap_or_default());
+        Self { db, access, lkg_virtual_state }
     }
 
     pub fn clone_with_new_cache(&self) -> Self {
-        Self::new(Arc::clone(&self.db))
+        Self::new(self.db.clone(), self.lkg_virtual_state.clone())
     }
 
     pub fn is_initialized(&self) -> StoreResult<bool> {
@@ -126,6 +181,7 @@ impl DbVirtualStateStore {
     }
 
     pub fn set_batch(&mut self, batch: &mut WriteBatch, state: Arc<VirtualState>) -> StoreResult<()> {
+        self.lkg_virtual_state.store(state.clone()); // Keep the LKG cache up-to-date
         self.access.write(BatchDbWriter::new(batch), &state)
     }
 }
@@ -138,6 +194,7 @@ impl VirtualStateStoreReader for DbVirtualStateStore {
 
 impl VirtualStateStore for DbVirtualStateStore {
     fn set(&mut self, state: Arc<VirtualState>) -> StoreResult<()> {
+        self.lkg_virtual_state.store(state.clone()); // Keep the LKG cache up-to-date
         self.access.write(DirectDbWriter::new(&self.db), &state)
     }
 }

consensus/src/pipeline/mod.rs
@@ -5,60 +5,5 @@ pub mod monitor;
 pub mod pruning_processor;
 pub mod virtual_processor;
 
-use std::sync::atomic::{AtomicU64, Ordering};
-
-#[derive(Default)]
-pub struct ProcessingCounters {
-    pub blocks_submitted: AtomicU64,
-    pub header_counts: AtomicU64,
-    pub dep_counts: AtomicU64,
-    pub mergeset_counts: AtomicU64,
-    pub body_counts: AtomicU64,
-    pub txs_counts: AtomicU64,
-    pub chain_block_counts: AtomicU64,
-    pub mass_counts: AtomicU64,
-}
-
-impl ProcessingCounters {
-    pub fn snapshot(&self) -> ProcessingCountersSnapshot {
-        ProcessingCountersSnapshot {
-            blocks_submitted: self.blocks_submitted.load(Ordering::Relaxed),
-            header_counts: self.header_counts.load(Ordering::Relaxed),
-            dep_counts: self.dep_counts.load(Ordering::Relaxed),
-            mergeset_counts: self.mergeset_counts.load(Ordering::Relaxed),
-            body_counts: self.body_counts.load(Ordering::Relaxed),
-            txs_counts: self.txs_counts.load(Ordering::Relaxed),
-            chain_block_counts: self.chain_block_counts.load(Ordering::Relaxed),
-            mass_counts: self.mass_counts.load(Ordering::Relaxed),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct ProcessingCountersSnapshot {
-    pub blocks_submitted: u64,
-    pub header_counts: u64,
-    pub dep_counts: u64,
-    pub mergeset_counts: u64,
-    pub body_counts: u64,
-    pub txs_counts: u64,
-    pub chain_block_counts: u64,
-    pub mass_counts: u64,
-}
-
-impl core::ops::Sub for &ProcessingCountersSnapshot {
-    type Output = ProcessingCountersSnapshot;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        Self::Output {
-            blocks_submitted: self.blocks_submitted.checked_sub(rhs.blocks_submitted).unwrap_or_default(),
-            header_counts: self.header_counts.checked_sub(rhs.header_counts).unwrap_or_default(),
-            dep_counts: self.dep_counts.checked_sub(rhs.dep_counts).unwrap_or_default(),
-            mergeset_counts: self.mergeset_counts.checked_sub(rhs.mergeset_counts).unwrap_or_default(),
-            body_counts: self.body_counts.checked_sub(rhs.body_counts).unwrap_or_default(),
-            txs_counts: self.txs_counts.checked_sub(rhs.txs_counts).unwrap_or_default(),
-            chain_block_counts: self.chain_block_counts.checked_sub(rhs.chain_block_counts).unwrap_or_default(),
-            mass_counts: self.mass_counts.checked_sub(rhs.mass_counts).unwrap_or_default(),
-        }
-    }
-}
+/// Re-export from consensus core
+pub use kaspa_consensus_core::api::counters::{ProcessingCounters, ProcessingCountersSnapshot};

rpc/core/src/model/message.rs
@@ -1,6 +1,6 @@
 use crate::model::*;
 use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
-use kaspa_consensus_core::block_count::BlockCount;
+use kaspa_consensus_core::api::stats::BlockCount;
 use kaspa_core::debug;
 use kaspa_notify::subscription::{single::UtxosChangedSubscription, Command};
 use serde::{Deserialize, Serialize};

Kash-Protocol/rusty-kash

consensus/src/model/stores/virtual_state.rs
@@ -1,10 +1,13 @@
+use std::ops::Deref;
 use std::sync::Arc;
 
+use arc_swap::ArcSwap;
+use kash_consensus_core::api::stats::VirtualStateStats;
 use kash_consensus_core::{
     block::VirtualStateApproxId, coinbase::BlockRewardData, config::genesis::GenesisBlock, tx::TransactionId,
     utxo::utxo_diff::UtxoDiff, BlockHashMap, BlockHashSet, HashMapCustomHasher,
 };
-use kash_database::prelude::{BatchDbWriter, CachedDbItem, DirectDbWriter};
+use kash_database::prelude::{BatchDbWriter, CachedDbItem, DirectDbWriter, StoreResultExtensions};
 use kash_database::prelude::{CachePolicy, StoreResult};
 use kash_database::prelude::{StoreError, DB};
 use kash_database::registry::DatabaseStorePrefixes;
@@ -77,6 +80,53 @@ impl VirtualState {
     }
 }
 
+impl From<&VirtualState> for VirtualStateStats {
+    fn from(state: &VirtualState) -> Self {
+        Self {
+            num_parents: state.parents.len() as u32,
+            daa_score: state.daa_score,
+            bits: state.bits,
+            past_median_time: state.past_median_time,
+        }
+    }
+}
+
+/// Represents the "last known good" virtual state. To be used by any logic which does not want to wait
+/// for a possible virtual state write to complete but can rather settle with the last known state
+#[derive(Clone, Default)]
+pub struct LkgVirtualState {
+    inner: Arc<ArcSwap<VirtualState>>,
+}
+
+/// Guard for accessing the last known good virtual state (lock-free)
+/// It's a simple newtype over arc_swap::Guard just to avoid explicit dependency
+pub struct LkgVirtualStateGuard(arc_swap::Guard<Arc<VirtualState>>);
+
+impl Deref for LkgVirtualStateGuard {
+    type Target = Arc<VirtualState>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl LkgVirtualState {
+    /// Provides a temporary borrow to the last known good virtual state.
+    pub fn load(&self) -> LkgVirtualStateGuard {
+        LkgVirtualStateGuard(self.inner.load())
+    }
+
+    /// Loads the last known good virtual state.
+    pub fn load_full(&self) -> Arc<VirtualState> {
+        self.inner.load_full()
+    }
+
+    // Kept private in order to make sure it is only updated by DbVirtualStateStore
+    fn store(&self, virtual_state: Arc<VirtualState>) {
+        self.inner.store(virtual_state)
+    }
+}
+
 /// Used in order to group virtual related stores under a single lock
 pub struct VirtualStores {
     pub state: DbVirtualStateStore,
@@ -84,9 +134,9 @@ pub struct VirtualStores {
 }
 
 impl VirtualStores {
-    pub fn new(db: Arc<DB>, utxoset_cache_policy: CachePolicy) -> Self {
+    pub fn new(db: Arc<DB>, lkg_virtual_state: LkgVirtualState, utxoset_cache_policy: CachePolicy) -> Self {
         Self {
-            state: DbVirtualStateStore::new(db.clone()),
+            state: DbVirtualStateStore::new(db.clone(), lkg_virtual_state),
             utxo_set: DbUtxoSetStore::new(db, utxoset_cache_policy, DatabaseStorePrefixes::VirtualUtxoset.into()),
         }
     }
@@ -106,15 +156,20 @@ pub trait VirtualStateStore: VirtualStateStoreReader {
 pub struct DbVirtualStateStore {
     db: Arc<DB>,
     access: CachedDbItem<Arc<VirtualState>>,
+    /// The "last known good" virtual state
+    lkg_virtual_state: LkgVirtualState,
 }
 
 impl DbVirtualStateStore {
-    pub fn new(db: Arc<DB>) -> Self {
-        Self { db: Arc::clone(&db), access: CachedDbItem::new(db, DatabaseStorePrefixes::VirtualState.into()) }
+    pub fn new(db: Arc<DB>, lkg_virtual_state: LkgVirtualState) -> Self {
+        let access = CachedDbItem::new(db.clone(), DatabaseStorePrefixes::VirtualState.into());
+        // Init the LKG cache from DB store data
+        lkg_virtual_state.store(access.read().unwrap_option().unwrap_or_default());
+        Self { db, access, lkg_virtual_state }
     }
 
     pub fn clone_with_new_cache(&self) -> Self {
-        Self::new(Arc::clone(&self.db))
+        Self::new(self.db.clone(), self.lkg_virtual_state.clone())
     }
 
     pub fn is_initialized(&self) -> StoreResult<bool> {
@@ -126,6 +181,7 @@ impl DbVirtualStateStore {
     }
 
     pub fn set_batch(&mut self, batch: &mut WriteBatch, state: Arc<VirtualState>) -> StoreResult<()> {
+        self.lkg_virtual_state.store(state.clone()); // Keep the LKG cache up-to-date
         self.access.write(BatchDbWriter::new(batch), &state)
     }
 }
@@ -138,6 +194,7 @@ impl VirtualStateStoreReader for DbVirtualStateStore {
 
 impl VirtualStateStore for DbVirtualStateStore {
     fn set(&mut self, state: Arc<VirtualState>) -> StoreResult<()> {
+        self.lkg_virtual_state.store(state.clone()); // Keep the LKG cache up-to-date
         self.access.write(DirectDbWriter::new(&self.db), &state)
     }
 }

consensus/src/pipeline/mod.rs
@@ -5,60 +5,5 @@ pub mod monitor;
 pub mod pruning_processor;
 pub mod virtual_processor;
 
-use std::sync::atomic::{AtomicU64, Ordering};
-
-#[derive(Default)]
-pub struct ProcessingCounters {
-    pub blocks_submitted: AtomicU64,
-    pub header_counts: AtomicU64,
-    pub dep_counts: AtomicU64,
-    pub mergeset_counts: AtomicU64,
-    pub body_counts: AtomicU64,
-    pub txs_counts: AtomicU64,
-    pub chain_block_counts: AtomicU64,
-    pub mass_counts: AtomicU64,
-}
-
-impl ProcessingCounters {
-    pub fn snapshot(&self) -> ProcessingCountersSnapshot {
-        ProcessingCountersSnapshot {
-            blocks_submitted: self.blocks_submitted.load(Ordering::Relaxed),
-            header_counts: self.header_counts.load(Ordering::Relaxed),
-            dep_counts: self.dep_counts.load(Ordering::Relaxed),
-            mergeset_counts: self.mergeset_counts.load(Ordering::Relaxed),
-            body_counts: self.body_counts.load(Ordering::Relaxed),
-            txs_counts: self.txs_counts.load(Ordering::Relaxed),
-            chain_block_counts: self.chain_block_counts.load(Ordering::Relaxed),
-            mass_counts: self.mass_counts.load(Ordering::Relaxed),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct ProcessingCountersSnapshot {
-    pub blocks_submitted: u64,
-    pub header_counts: u64,
-    pub dep_counts: u64,
-    pub mergeset_counts: u64,
-    pub body_counts: u64,
-    pub txs_counts: u64,
-    pub chain_block_counts: u64,
-    pub mass_counts: u64,
-}
-
-impl core::ops::Sub for &ProcessingCountersSnapshot {
-    type Output = ProcessingCountersSnapshot;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        Self::Output {
-            blocks_submitted: self.blocks_submitted.checked_sub(rhs.blocks_submitted).unwrap_or_default(),
-            header_counts: self.header_counts.checked_sub(rhs.header_counts).unwrap_or_default(),
-            dep_counts: self.dep_counts.checked_sub(rhs.dep_counts).unwrap_or_default(),
-            mergeset_counts: self.mergeset_counts.checked_sub(rhs.mergeset_counts).unwrap_or_default(),
-            body_counts: self.body_counts.checked_sub(rhs.body_counts).unwrap_or_default(),
-            txs_counts: self.txs_counts.checked_sub(rhs.txs_counts).unwrap_or_default(),
-            chain_block_counts: self.chain_block_counts.checked_sub(rhs.chain_block_counts).unwrap_or_default(),
-            mass_counts: self.mass_counts.checked_sub(rhs.mass_counts).unwrap_or_default(),
-        }
-    }
-}
+/// Re-export from consensus core
+pub use kash_consensus_core::api::counters::{ProcessingCounters, ProcessingCountersSnapshot};

rpc/core/src/model/message.rs
@@ -1,6 +1,6 @@
 use crate::model::*;
 use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
-use kash_consensus_core::block_count::BlockCount;
+use kash_consensus_core::api::stats::BlockCount;
 use kash_core::debug;
 use kash_notify::subscription::{single::UtxosChangedSubscription, Command};
 use serde::{Deserialize, Serialize};