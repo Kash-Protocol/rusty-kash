2215367e9bd1c7a3a182510d70164c6dee6966af
D-Stacks <78099568+d-stacks@users.noreply.github.com>
2024-01-24 05:14:46 +0800

    remove redundant files. (#365)

--- indexes/processor/src/connection.rs
+++ indexes/processor/src/connection.rs
@@ -1,3 +1,1 @@
-use crate::notification::Notification;
 
-pub type IndexChannelConnection = kash_notify::connection::ChannelConnection<Notification>;
--- indexes/processor/src/notifier.rs
+++ indexes/processor/src/notifier.rs
@@ -1,4 +1,1 @@
-use crate::notification::Notification;
-use kash_notify::{connection::ChannelConnection, notifier::Notifier};
 
-pub type IndexNotifier = Notifier<Notification, ChannelConnection<Notification>>;
