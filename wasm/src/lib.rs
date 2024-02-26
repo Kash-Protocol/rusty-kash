kaspanet/rusty-kaspa

Misc doc updates (#407)
* misc doc updates

* Update JS code and add additional comments

wasm/src/lib.rs
@@ -13,11 +13,11 @@ codebase within JavaScript environments such as Node.js and Web Browsers.
 
 ## Documentation
 
-- [**integrating with Kaspa** guide](https://kaspa-mdbook.aspectron.com/)
+- [**integrating with Kaspa** guide](https://kaspa.aspectron.org/)
 - [**Rustdoc** documentation](https://docs.rs/kaspa-wasm/latest/kaspa-wasm)
-- [**JSDoc** documentation](https://aspectron.com/docs/kaspa-wasm/)
+- [**JSDoc** documentation](https://kaspa.aspectron.org/jsdoc/)
 
-Please note that while WASM directly binds JacaScript and Rust resources, their names on JavaScript side
+Please note that while WASM directly binds JavaScript and Rust resources, their names on JavaScript side
 are different from their name in Rust as they conform to the 'camelCase' convention in JavaScript and
 to the 'snake_case' convention in Rust.
 
@@ -26,23 +26,42 @@ to the 'snake_case' convention in Rust.
 The APIs are currently separated into the following groups (this will be expanded in the future):
 
 - **Transaction API** — Bindings for primitives related to transactions.
-This includes basic primitives related to consensus transactions, as well as
-`MutableTransaction` and `VirtualTransaction` primitives usable for
-transaction creation.
-
+- **RPC API** — [RPC interface bindings](rpc) for the Kaspa node using WebSocket (wRPC) connections.
 - **Wallet API** — API for async core wallet processing tasks.
 
-- **RPC API** — [RPC interface bindings](rpc) for the Kaspa node using WebSocket connections.
-Compatible with Rusty Kaspa as well as with the Golang node (kaspad) via the `kaspa-wrpc-proxy`
-WebSocket / gRPC proxy (located in `rpc/wrpc/proxy`).
+## NPM Modules
+
+For JavaScript / TypeScript environments, there are two
+available NPM modules:
+
+- <https://www.npmjs.com/package/kaspa>
+- <https://www.npmjs.com/package/kaspa-wasm>
+
+The `kaspa-wasm` module is a pure WASM32 module that includes
+the entire wallet framework, but does not support RPC due to an absence
+of a native WebSocket in NodeJs environment, while
+the `kaspa` module includes `isomorphic-ws` dependency simulating
+the W3C WebSocket and thus supports RPC.
+
+## Examples
+
+JavaScript examples for using this framework can be found at:
+<https://github.com/kaspanet/rusty-kaspa/tree/master/wasm/nodejs>
+
+## WASM32 Binaries
+
+For pre-built browser-compatible WASM32 redistributables of this
+framework please see the releases section of the Rusty Kaspa
+repository at <https://github.com/kaspanet/rusty-kaspa/releases>.
 
 ## Using RPC
 
-**NODEJS:** To use WASM RPC client in the Node.js environment, you need to introduce a W3C WebSocket object
-before loading the WASM32 library. You can use any Node.js module that exposes a W3C-compatible
-WebSocket implementation. Two of such modules are [WebSocket](https://www.npmjs.com/package/websocket)
-(provides a custom implementation) and [isomorphic-ws](https://www.npmjs.com/package/isomorphic-ws)
-(built on top of the ws WebSocket module).
+**NODEJS:** If you are building from source, to use WASM RPC client in the NodeJS environment,
+you need to introduce a W3C WebSocket object before loading the WASM32 library. You can use
+any Node.js module that exposes a W3C-compatible WebSocket implementation. Two of such modules
+are [WebSocket](https://www.npmjs.com/package/websocket) (provides a custom implementation)
+and [isomorphic-ws](https://www.npmjs.com/package/isomorphic-ws) (built on top of the ws
+WebSocket module).
 
 You can use the following shims:
 
@@ -62,6 +81,7 @@ globalThis.WebSocket = require('isomorphic-ws');
             import * as kaspa_wasm from './kaspa/kaspa-wasm.js';
             (async () => {
                 const kaspa = await kaspa_wasm.default('./kaspa/kaspa-wasm_bg.wasm');
+                // ...
             })();
         </script>
     </head>
@@ -73,24 +93,33 @@ globalThis.WebSocket = require('isomorphic-ws');
 
 ```javascript
 // W3C WebSocket module shim
-globalThis.WebSocket = require('websocket').w3cwebsocket;
+// this is provided by NPM `kaspa` module and is only needed
+// if you are building WASM libraries for NodeJS from source
+// globalThis.WebSocket = require('websocket').w3cwebsocket;
 
-let {RpcClient,Encoding,init_console_panic_hook,defer} = require('./kaspa-rpc');
-// init_console_panic_hook();
+let {RpcClient,Encoding,initConsolePanicHook} = require('./kaspa-rpc');
 
-let rpc = new RpcClient(Encoding.Borsh,"ws://127.0.0.1:17110");
+// enabling console panic hooks allows WASM to print panic details to console
+// initConsolePanicHook();
+// enabling browser panic hooks will create a full-page DIV with panic details
+// this is useful for mobile devices where console is not available
+// initBrowserPanicHook();
+
+// if port is not specified, it will use the default port for the specified network
+const rpc = new RpcClient("127.0.0.1", Encoding.Borsh, "testnet-10");
 
 (async () => {
-    await rpc.connect();
-
-    let info = await rpc.getInfo();
-    console.log(info);
-
-    await rpc.disconnect();
+    try {
+        await rpc.connect();
+        let info = await rpc.getInfo();
+        console.log(info);
+    } finally {
+        await rpc.disconnect();
+    }
 })();
 ```
 
-For more details, please follow the [**integrating with Kaspa**](https://kaspa-mdbook.aspectron.com/) guide.
+For more details, please follow the [**integrating with Kaspa**](https://kaspa.aspectron.org/) guide.
 
 */
 

Kash-Protocol/rusty-kash

wasm/src/lib.rs
@@ -17,7 +17,7 @@ codebase within JavaScript environments such as Node.js and Web Browsers.
 - [**Rustdoc** documentation](https://docs.rs/kash-wasm/latest/kash-wasm)
 - [**JSDoc** documentation](https://aspectron.com/docs/kash-wasm/)
 
-Please note that while WASM directly binds JacaScript and Rust resources, their names on JavaScript side
+Please note that while WASM directly binds JavaScript and Rust resources, their names on JavaScript side
 are different from their name in Rust as they conform to the 'camelCase' convention in JavaScript and
 to the 'snake_case' convention in Rust.
 
@@ -26,23 +26,42 @@ to the 'snake_case' convention in Rust.
 The APIs are currently separated into the following groups (this will be expanded in the future):
 
 - **Transaction API** — Bindings for primitives related to transactions.
-This includes basic primitives related to consensus transactions, as well as
-`MutableTransaction` and `VirtualTransaction` primitives usable for
-transaction creation.
-
+- **RPC API** — [RPC interface bindings](rpc) for the Kaspa node using WebSocket (wRPC) connections.
 - **Wallet API** — API for async core wallet processing tasks.
 
-- **RPC API** — [RPC interface bindings](rpc) for the Kash node using WebSocket connections.
-Compatible with Rusty Kash as well as with the Golang node (kashd) via the `kash-wrpc-proxy`
-WebSocket / gRPC proxy (located in `rpc/wrpc/proxy`).
+## NPM Modules
+
+For JavaScript / TypeScript environments, there are two
+available NPM modules:
+
+- <https://www.npmjs.com/package/kaspa>
+- <https://www.npmjs.com/package/kaspa-wasm>
+
+The `kaspa-wasm` module is a pure WASM32 module that includes
+the entire wallet framework, but does not support RPC due to an absence
+of a native WebSocket in NodeJs environment, while
+the `kaspa` module includes `isomorphic-ws` dependency simulating
+the W3C WebSocket and thus supports RPC.
+
+## Examples
+
+JavaScript examples for using this framework can be found at:
+<https://github.com/kaspanet/rusty-kaspa/tree/master/wasm/nodejs>
+
+## WASM32 Binaries
+
+For pre-built browser-compatible WASM32 redistributables of this
+framework please see the releases section of the Rusty Kaspa
+repository at <https://github.com/kaspanet/rusty-kaspa/releases>.
 
 ## Using RPC
 
-**NODEJS:** To use WASM RPC client in the Node.js environment, you need to introduce a W3C WebSocket object
-before loading the WASM32 library. You can use any Node.js module that exposes a W3C-compatible
-WebSocket implementation. Two of such modules are [WebSocket](https://www.npmjs.com/package/websocket)
-(provides a custom implementation) and [isomorphic-ws](https://www.npmjs.com/package/isomorphic-ws)
-(built on top of the ws WebSocket module).
+**NODEJS:** If you are building from source, to use WASM RPC client in the NodeJS environment,
+you need to introduce a W3C WebSocket object before loading the WASM32 library. You can use
+any Node.js module that exposes a W3C-compatible WebSocket implementation. Two of such modules
+are [WebSocket](https://www.npmjs.com/package/websocket) (provides a custom implementation)
+and [isomorphic-ws](https://www.npmjs.com/package/isomorphic-ws) (built on top of the ws
+WebSocket module).
 
 You can use the following shims:
 
@@ -62,6 +81,7 @@ globalThis.WebSocket = require('isomorphic-ws');
             import * as kash_wasm from './kash/kash-wasm.js';
             (async () => {
                 const kash = await kash_wasm.default('./kash/kash-wasm_bg.wasm');
+                // ...
             })();
         </script>
     </head>
@@ -73,20 +93,29 @@ globalThis.WebSocket = require('isomorphic-ws');
 
 ```javascript
 // W3C WebSocket module shim
-globalThis.WebSocket = require('websocket').w3cwebsocket;
+// this is provided by NPM `kaspa` module and is only needed
+// if you are building WASM libraries for NodeJS from source
+// globalThis.WebSocket = require('websocket').w3cwebsocket;
 
-let {RpcClient,Encoding,init_console_panic_hook,defer} = require('./kash-rpc');
-// init_console_panic_hook();
+let {RpcClient,Encoding,initConsolePanicHook} = require('./kash-rpc');
 
-let rpc = new RpcClient(Encoding.Borsh,"ws://127.0.0.1:17110");
+// enabling console panic hooks allows WASM to print panic details to console
+// initConsolePanicHook();
+// enabling browser panic hooks will create a full-page DIV with panic details
+// this is useful for mobile devices where console is not available
+// initBrowserPanicHook();
+
+// if port is not specified, it will use the default port for the specified network
+const rpc = new RpcClient("127.0.0.1", Encoding.Borsh, "testnet-10");
 
 (async () => {
-    await rpc.connect();
-
-    let info = await rpc.getInfo();
-    console.log(info);
-
-    await rpc.disconnect();
+    try {
+        await rpc.connect();
+        let info = await rpc.getInfo();
+        console.log(info);
+    } finally {
+        await rpc.disconnect();
+    }
 })();
 ```
 